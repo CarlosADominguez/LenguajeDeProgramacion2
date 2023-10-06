44c87ae6fd34d942c881e72dce5fa93a51b58d21
CarlosADominguez <145080908+CarlosADominguez@users.noreply.github.com>
2023-10-06 08:49:30 -0600

    En esta practica realizamos la herencia, basicamente creamos la superclase, y las dos subclases basandonos en Pokemon, estas subclases heredan de la superclase.

---
+++ Pokemon_Herencia/BatallaPokemon.java
@@ -0,0 +1,14 @@
+public class BatallaPokemon {
+
+    public static void main(String[] args) {
+     
+    
+    PokemonAgua psyduck = new PokemonAgua("Psyduck",10);
+    PokemonFuego moltres = new PokemonFuego("Moltres",10);
+    
+    psyduck.atacar("Confusion",moltres);
+    moltres.atacar("Giro fuego", psyduck);
+    
+    
+    }
+}
---
+++ Pokemon_Herencia/Pokemon.java
@@ -0,0 +1,36 @@
+public class Pokemon {
+    private int hp;
+    private String nombre,tipo;
+    
+    public Pokemon (String nombre, String tipo, int HP){
+       this.nombre=nombre;
+       this.tipo=tipo;
+       this.hp=100; 
+    }
+    public int getHP(){
+        return hp;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+    private void calculaDanio(int danio) {
+    this.hp -= danio;
+    System.out.printf("%s recibe %d puntos de danio\n", this.getNombre(), danio);
+    System.out.printf("%s tiene %d puntos de vida\n", this.getNombre(), hp);
+    }
+    
+    public void recibirAtaque(String movimiento) {
+    System.out.printf("%s recibe ATK %s\n", this.getNombre(),movimiento);
+    calculaDanio((int) (Math.random() * 10) + 1);
+    }
+    
+    public void atacar(String movimiento, Pokemon pokemon) {
+    System.out.printf("%s ataca a %s con %s\n", this.getNombre(),
+    pokemon.getNombre(), movimiento);
+    pokemon.recibirAtaque(movimiento);
+    }   
+    
+}
